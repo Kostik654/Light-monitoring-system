0141f23b48148b3b8c3f031cf72ba11e461ef361
Kostik654 <kostia.andreew2015@yandex.ru>
2024-10-10 17:21:31 +0300

    logs to file update 1

--- backend_engine/logger.py
+++ backend_engine/logger.py
@@ -3,6 +3,8 @@ import datetime
 from tgbot_post import send_telegram_message
 from ms_configs import TgBotPosterData
 
+log_path: str = "/var/log/monsys/monitor.log"
+
 
 def LogItOut(message_: str,
              for_tg: bool = False,
@@ -19,6 +21,13 @@ def LogItOut(message_: str,
     if not just_for_tg:
         message_ = message_.replace('\n', '\t')
         if add_timestamp:
-            print(message_ + f"\t{datetime.datetime.now()}")
+            print(f"{message_}\t{datetime.datetime.now()}")
         else:
             print(message_ )
+
+        # to file
+        try:
+            with open(log_path, 'a') as file:
+                file.write(f"{message_}\t{datetime.datetime.now()}")
+        except Exception as e:
+            print(f"Exception occurred while reading {log_path} file [Adding strs]\n{e.__str__()}")
--- install_as_root.sh
+++ install_as_root.sh
@@ -3,10 +3,17 @@
 set -e
 
 rm -rf /etc/monsys
+rm -rf /var/log/monsys
+
 mkdir /etc/monsys
+mkdir /var/log/monsys
+
+touch /var/log/monsys/monitor.log
 
 cp ./templates/jobs_list.template /etc/monsys/jobs_list
 cp ./templates/env.template /etc/monsys/.env
+
+docker rm -f monitoring-system
 
 docker-compose up -d --build
 
