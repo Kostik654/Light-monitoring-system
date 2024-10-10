e4f412cade8f3b459d1f57c08dfb8b3f0b0bb457
Kostik654 <kostia.andreew2015@yandex.ru>
2024-10-10 22:13:41 +0300

    Fixes 2a

--- backend_engine/export_app.py
+++ backend_engine/export_app.py
@@ -1,9 +1,12 @@
 from flask import Flask, Response
 import time
 
+from logger import LogItOut
+
 app = Flask(__name__)
 
-@app.route('/monitoring')
-async def metrics():
 
+@app.route('/monitoring')
+def metrics():
+    LogItOut(message_='Monitoring endpoint accessed')
     return {"status": "OK"}
