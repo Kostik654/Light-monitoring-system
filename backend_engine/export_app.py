f800d025bb1c2f38b84cf593d485367d07a0e290
Kostik654 <kostia.andreew2015@yandex.ru>
2024-10-10 22:29:29 +0300

    Fixes 4 final

--- backend_engine/export_app.py
+++ backend_engine/export_app.py
@@ -5,4 +5,21 @@ app = Quart(__name__)
 
 @app.route('/monitoring')
 async def metrics():
-    return jsonify({"status": "OK"})
+
+    #  TEST DATA
+    
+    data = {
+        "metric_1": 123,
+        "metric_2": 456,
+        "metric_3": 789
+    }
+
+    prometheus_data = f"""
+    # HELP metric_1 Description of metric_1
+    metric_1 {data['metric_1']}
+    # HELP metric_2 Description of metric_2
+    metric_2 {data['metric_2']}
+    # HELP metric_3 Description of metric_3
+    metric_3 {data['metric_3']}
+    """
+    return prometheus_data, 200, {'Content-Type': 'text/plain; version=0.0.4'}
